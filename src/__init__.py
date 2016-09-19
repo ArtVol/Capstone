16b36e96fd6664c7ce6e4f00256ae71671c8439e
Alexey Kovalov <alexey.kovalov@cloudbear.ru>
2016-09-19 13:49:47 +0300

    Add settings

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,5 @@ from requester import asker
 from renders import render
 from advisor import clothesAdvisor
 from advisor import voice
+from settings import settings_reader
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
---
+++ src/settings/settings_reader.py
@@ -0,0 +1,20 @@
+import os
+__author__ = 'Alex'
+import re
+
+def get_coords(file_sett=os.path.join('settings', 'setting')):
+    sett = get_settings(file_sett)['location']
+    # location: lat=55.684647, lon=37.340472;
+    lat = float(re.findall(r'lat=(\d+(?:\.?\d+))', sett)[0])
+    lon = float(re.findall(r'lon=(\d+(?:\.?\d+))', sett)[0])
+
+    return {'lat': lat, 'lon': lon}
+
+def get_settings(file_sett=os.path.join('settings', 'setting')):
+    with open(file_sett) as f:
+        r_str_data = f.read()
+    r_data = {i.split(':')[0]: i.split(':')[1]
+              for i in re.sub(r'\s*', '', r_str_data).split(';')
+              if i}
+
+    return r_data
