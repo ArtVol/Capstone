716b82fb3b2c96f90a9df5ea3c0d3c17bbeda4d4
Artem Volohatiy <artem.volohatiy@cloudbear.ru>
2016-09-21 09:56:58 +0300

    Fix audio exception

--- src/speak.py
+++ src/speak.py
@@ -13,14 +13,15 @@ def say():
     v = voice.Voice()
     data = asker.get_weather_by_coords(current=False)['list']
     text = v.sirena(weather.get_today_weather(data))
-    v.speak(text)
+    filename = "audio" + time.strftime("%Y-%m-%d_%H_%M_%S") + ".mp3"
+    v.speak(text,filename)
     time.sleep(10)
-
+    os.system("rm " + filename)
 
 def key_press(event):
     say()
 
-    print('say', time.strftime("%Y-%m-%d %H:%M:%S"))
+    print 'say', time.strftime("%Y-%m-%d %H:%M:%S")
 
 root = tk.Tk()
 root.bind('<KeyPress>', key_press)
